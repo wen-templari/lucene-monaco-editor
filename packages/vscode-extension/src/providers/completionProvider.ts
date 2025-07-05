ceac984a04ec2cca10f783d6ba1a05bd8e7254c7
Clas Wen <clas.wen@icloud.com>
2025-07-05 17:55:12 +0800

    fix lint

--- packages/vscode-extension/src/providers/completionProvider.ts
+++ packages/vscode-extension/src/providers/completionProvider.ts
@@ -5,8 +5,8 @@ export class CompletionProvider implements vscode.CompletionItemProvider {
   provideCompletionItems(
     document: vscode.TextDocument,
     position: vscode.Position,
-    token: vscode.CancellationToken,
-    context: vscode.CompletionContext
+    _token: vscode.CancellationToken,
+    _context: vscode.CompletionContext
   ): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
     
     // Get the current line and text before cursor
